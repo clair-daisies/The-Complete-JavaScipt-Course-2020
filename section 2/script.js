f54e6f028183e77a91f30f4ad09d95db1520abd7
delfina <delfina.ghimire@gmail.com>
2020-02-14 18:34:09 +0545

    Section 2

---
+++ section 2/script.js
@@ -0,0 +1,39 @@
+/***************VARIABLE MUTATION AND TYPE COERCION 
+ var firstName = "Mary";
+var age = 23;
+console.log(name + "is " + age + " years old");
+
+//  TYPE COERCION: JS automatically changes datatype from one to another as needed//
+
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " years old " +
+    job +
+    ". Is she married? " +
+    isMarried
+);
+
+// VARIABLE MUTATION: to change the value of variable //
+age = "twenty-three";
+job = "dancer";
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " years old " +
+    job +
+    ". Is she married? " +
+    isMarried
+);
+
+var lastName = prompt("What is her last name?");
+console.log(firstName + " " + lastName);
+*/
+
+/****************************** BASIC OPERATORS */
